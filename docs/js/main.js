370eee0e4f0a8bb00a94ce77af463db63fd25cda
Juan Carlos Lugo <46272126+juanlugodegouveia@users.noreply.github.com>
2021-11-10 21:10:57 +0000

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -52,6 +52,15 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
+//     Sweet Alert
+    Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Your work has been saved',
+  showConfirmButton: false,
+  timer: 1500
+})
+    
     
 
 })(jQuery);
